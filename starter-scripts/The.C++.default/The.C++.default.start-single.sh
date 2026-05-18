7cecd1f6245944979c4def013447569fb3223eb8
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-05-18 20:17:44 +0300

    Erasing redundant print

--- starter-scripts/The.C++.default/The.C++.default.start-single.sh
+++ starter-scripts/The.C++.default/The.C++.default.start-single.sh
@@ -2,11 +2,6 @@
 
 . starter-scripts/The.C++.default/The.C++.default.common.sh
 
-echo "COMMAND: docker run --rm -it \
-    --name ${dockerContainerName} \
-    --hostname ${dockerContainerHostName} \
-    $@ ${dockerImageName}"
-
 docker run --rm -it \
     --name ${dockerContainerName} \
     --hostname ${dockerContainerHostName} \
