f8b5691958d2b75fd76fd058f1fae7fa4a470d02
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-06-18 20:38:49 +0300

    Making the default C++ container image name static

--- starter-scripts/The.C++.default/The.C++.default.common.sh
+++ starter-scripts/The.C++.default/The.C++.default.common.sh
@@ -2,12 +2,13 @@ dockerFilesDir="Dockerfiles"
 
 myDockerfile=${dockerFilesDir}/The.C++.default.Dockerfile
 
-dockerContainerHostName=the-cxx-default
+dockerDefaultImageNamePrefix=the-cxx-default
+dockerContainerHostName=${dockerDefaultImageNamePrefix}
 
 if [[ ! -z ${CLAUDE_CONTAINER_NAME} ]]
 then
     dockerContainerHostName="${CLAUDE_CONTAINER_NAME}"
 fi
 
-dockerImageName=${dockerContainerHostName}-docker-image
+dockerImageName=${dockerDefaultImageNamePrefix}-docker-image
 dockerContainerName=${dockerContainerHostName}-docker-container
